fba56e81709446392b1ae0dc7f7ccedab3b9ba55
ibuibu <ibuibu69@gmail.com>
2021-02-06 19:46:54 +0900

    Change Shortcut Key.

--- content.js
+++ content.js
@@ -1,6 +1,5 @@
- 
     document.onkeydown = function (e){
-        if(e.keyCode == 83 && event.ctrlKey){//s key
+        if(e.keyCode == 83 && event.ctrlKey && event.shiftKey){
             const s = document.querySelector('.gLFyf');
             const val = s.value;
             s.select();
